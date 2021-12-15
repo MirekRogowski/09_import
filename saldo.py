4269c3736b62b1d18cca31f9bc3991af58ff2b17
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-15 13:09:19 +0100

    Update saldo.py
    saldo v2

--- saldo.py
+++ saldo.py
@@ -1,18 +1,7 @@
 from helper import *
 
-if sys.argv[0] == "saldo.py":
-    accountancy.open_data(sys.argv[1])
-    accountancy.transform_data()
-    accountancy.logs.append(["saldo", sys.argv[2], sys.argv[3]])
-    accountancy.balance += int(sys.argv[2])
-    accountancy.writa_data("saldo","01zapis.txt")
-
-# print(accountancy.balance)
-# print(accountancy.logs)
-# print(accountancy.warehouse)
-
-#
-# if (len(sys.argv) == 4 and sys.argv[1]=="saldo") or (len(sys.argv) == 2 and sys.argv[1]=="saldo"):
-#     accountancy.logs.append(sys.argv[1:])
-#     accountancy.balance += int(sys.argv[2])
-#     accountancy.writa_data("zapis.txt")
+accountancy.open_data(sys.argv[1])
+accountancy.transform_data()
+accountancy.logs.append(["saldo", sys.argv[2], sys.argv[3]])
+accountancy.balance += int(sys.argv[2])
+accountancy.writa_data("saldo","01zapis.txt")
