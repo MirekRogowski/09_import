cfdb35603a6136875fef31991f4c6167b3ce535f
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-15 13:09:11 +0100

    Update sprzedaz.py
    sprzedaz v2

--- sprzedaz.py
+++ sprzedaz.py
@@ -1,7 +1,6 @@
 from helper import *
 
-if sys.argv[0] == "sprzedaz.py":
-    accountancy.open_data(sys.argv[1])
-    accountancy.transform_data()
-    accountancy.get_sale(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
-    accountancy.writa_data("sprzedaz", "01zapis.txt")
+accountancy.open_data(sys.argv[1])
+accountancy.transform_data()
+accountancy.get_sale(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+accountancy.writa_data("sprzedaz", "01zapis.txt")
