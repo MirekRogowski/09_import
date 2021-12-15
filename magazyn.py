36bb9e1da05bbf7a7484f987d301f2d1dc95dff7
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-15 13:09:31 +0100

    Update magazyn.py
    magazyn v2

--- magazyn.py
+++ magazyn.py
@@ -1,9 +1,7 @@
 from helper import *
 
-if sys.argv[0] == "magazyn.py":
-    accountancy.open_data(sys.argv[1])
-    accountancy.transform_data()
-    # accountancy.logs.append(sys.argv[2:])
-    accountancy.writa_data("magazyn", "01zapis.txt")
-    accountancy.stock_status(sys.argv[2:])
+accountancy.open_data(sys.argv[1])
+accountancy.transform_data()
+accountancy.writa_data("magazyn", "01zapis.txt")
+accountancy.stock_status(sys.argv[2:])
 
