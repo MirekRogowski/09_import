a5b7d26ebd90d4c24000165b74bff72f49b35c2a
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-15 13:07:36 +0100

    Update konto.py
    konto v2

--- konto.py
+++ konto.py
@@ -1,8 +1,7 @@
 from helper import *
 
-if sys.argv[0] == "konto.py":
-    accountancy.open_data(sys.argv[1])
-    accountancy.transform_data()
-    accountancy.logs.append(sys.argv[2:])
-    accountancy.writa_data("konto", "01zapis.txt")
+accountancy.open_data(sys.argv[1])
+accountancy.transform_data()
+accountancy.logs.append(sys.argv[2:])
+accountancy.writa_data("konto", "01zapis.txt")
 print(f"\nKonto: {accountancy.balance}")
