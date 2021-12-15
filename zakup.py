c8dd3f2d911fe090e175c3b9a156c2cb6a077efb
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-15 13:04:33 +0100

    Create zakup.py
    zakup

---
+++ zakup.py
@@ -0,0 +1,7 @@
+from helper import *
+
+if sys.argv[0] == "zakup.py":
+    accountancy.open_data(sys.argv[1])
+    accountancy.transform_data()
+    accountancy.get_buy(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+    accountancy.writa_data("zakup", "01zapis.txt")
