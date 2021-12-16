ab9fa212f789c2daddd15cec0b876eb9e22621d9
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-12-16 22:05:25 +0100

    Update konto.py
    konto v3

--- konto.py
+++ konto.py
@@ -1,7 +1,6 @@
 from helper import *
 
 accountancy.open_data(sys.argv[1])
-accountancy.transform_data()
-accountancy.logs.append(sys.argv[2:])
+accountancy.get_balance("konto", *sys.argv[2:])
 accountancy.writa_data("konto", "01zapis.txt")
 print(f"\nKonto: {accountancy.balance}")
