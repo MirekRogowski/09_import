54f3cd78a2c59aefc0c8d0ef43b662608338301c
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-12-16 22:05:02 +0100

    Update helper.py
    helper

--- helper.py
+++ helper.py
@@ -13,6 +13,8 @@ class Accountant:
         with open(filepath) as f:
             for line in f:
                 self.data.append(line.strip())
+        self.transform_data()
+        return
 
     def writa_data(self, action, filepath):
         with open(filepath, "w") as f:
@@ -31,72 +33,55 @@ class Accountant:
                     print(f"{prd_id} : brak poyzcji w magazynie")
         return
 
-
     def transform_data(self):
-        # transformed_data = []
+        transformed_data = []
         index = 0
         action_param = {"saldo": 2, "zakup": 3, "sprzedaz": 3}
         while index < len(self.data):
             if self.data[index] in action_param:
                 qty_param = action_param[self.data[index]]
                 action = self.data[index]
-                if action == "saldo":
-                    self.logs.append([action, self.data[index + 1], self.data[index + qty_param]])
-                    self.balance += int(self.data[index+1])
-                elif action == "zakup":
-                    self.logs.append([action, self.data[index + 1], self.data[index + 2], self.data[index + qty_param]])
-                    self.balance -= int(self.data[index + qty_param]) * int(self.data[index + qty_param])
-                    if self.data[index + 1] in self.warehouse:
-                        # add quntity to warehouse
-                        self.warehouse[self.data[index + 1]] += int(self.data[index + qty_param])
-                    else:
-                        # add item to warehouse
-                        self.warehouse[self.data[index + 1]] = self.data[index + qty_param]
-
-                elif action == "sprzedaz":
-                    self.logs.append([action, self.data[index + 1], self.data[index + 2], self.data[index + qty_param]])
-                    if (int(self.warehouse[self.data[index + 1]]) - int(self.data[index + qty_param])) < 0:
-                        print(f"\nChcesz sprzedać {self.data[index + qty_param]} sztuk {self.data[index + 1]}. "
-                              f" W magazynie jest: {self.warehouse[self.data[index + 1]]}\n ")
-                        return
-                    qty = int(self.warehouse[self.data[index + 1]])
-                    qty -= int(self.data[index + qty_param])
-                    self.warehouse[self.data[index + 1]] =qty
-                    self.balance += int(self.data[index + 2]) * int(self.data[index + qty_param])
                 index += 1
                 tmp = self.data[index:index+qty_param]
-                self.transformed_data.append([action, tmp])
+                if action == "saldo":
+                    self.get_balance(action, *tmp)
+                if action == "zakup":
+                    self.get_buy(action, *tmp)
+                if action == "sprzedaz":
+                    self.get_sale(action, *tmp)
+                transformed_data.append([action, tmp])
                 index += qty_param
             else:
-                print("Koniec programu")
                 break
-        return
+        return True
 
-    def get_balance(self, action):
-        new_balance = int(input("Podaj wartość salda: "))
-        self.balance += new_balance
-        comments = input("Komentarz: ")
+    def get_balance(self, action, new_balance, comments):
+        self.balance += int(new_balance)
         self.logs.append([action, new_balance, comments])
         return
 
-    def get_buy(self, prd_id, prd_prise, prd_qty):
+    def get_buy(self, action, prd_id, prd_prise, prd_qty):
+        prise = int(prd_prise)
+        qty = int(prd_qty)
         if self.balance <= 0:
             print(f"\nSaldo wynosi: {self.balance}. Nie można kupić towaru\n ")
             return
-        if prd_prise * prd_qty > self.balance:
+        if prise * qty > self.balance:
             print(f"\nNie mozna dokonac zakupu. Za małe saldo: {self.balance}\n")
             return
-        self.balance -= prd_prise * prd_qty
+        self.balance -= prise * qty
         if prd_id in self.warehouse:
             # add quntity to warehouse
-            self.warehouse[prd_id] += prd_qty
+            self.warehouse[prd_id] += qty
         else:
             # add item to warehouse
-            self.warehouse[prd_id] = prd_qty
-        self.logs.append(["zakup", prd_id, prd_prise, prd_qty])
+            self.warehouse[prd_id] = qty
+        self.logs.append([action, prd_id, prd_prise, qty])
         return
 
-    def get_sale(self, prd_id, prd_prise, prd_qty):
+    def get_sale(self, action, prd_id, prd_prise, prd_qty):
+        prise = int(prd_prise)
+        qty = int(prd_qty)
         if not self.warehouse:
             print("\nMagazyn jest pusty proszeę zakupić towar.\n")
             return
@@ -106,24 +91,20 @@ class Accountant:
             index = 0
             while index < len(self.warehouse):
             # for key in self.warehouse:
-                temp_data += temp_data.append(f"{key} - {self.warehouse[key]} sztuk")
+                temp_data += temp_data.append(f"{index} - {self.warehouse[index]} sztuk")
                 # print(f"{key} - {self.warehouse[key]} sztuk")
                 continue
             return
         # remove item from warehouse
-        if (self.warehouse[prd_id] - prd_qty) < 0:
-            print(f"\nChcesz sprzedać {prd_qty} {prd_id} w magazynie {self.warehouse[prd_id]}\n ")
+        if (self.warehouse[prd_id] - qty) < 0:
+            print(f"\nChcesz sprzedać {qty} {prd_id} w magazynie {self.warehouse[prd_id]}\n ")
             return
-        self.warehouse[prd_id] -= prd_qty
-        self.balance += prd_prise * prd_qty
-        self.logs.append(["sprzedaz", prd_id, prd_prise, prd_qty])
-
-
+        self.warehouse[prd_id] -= qty
+        self.balance += prise * qty
+        self.logs.append([action, prd_id, prd_prise, prd_qty])
 
 def review(first, last):
-    first = int(sys.argv[2])
-    last = int(sys.argv[3])
-    if first < 1 :
+    if first < 1:
       first = 1
     if last > len(accountancy.logs) :
       last = len(accountancy.logs)
@@ -134,5 +115,3 @@ def review(first, last):
 
 
 accountancy = Accountant()
-
-
