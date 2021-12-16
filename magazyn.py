cd8dec9d86fcff35267bd39e895a534e1bed028a
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2021-12-16 23:53:18 +0100

    Update magazyn.py
    magazyn

--- magazyn.py
+++ magazyn.py
@@ -1,7 +1,5 @@
 from helper import *
 
 accountancy.open_data(sys.argv[1])
-accountancy.transform_data()
-accountancy.writa_data("magazyn", "01zapis.txt")
 accountancy.stock_status(sys.argv[2:])
-
+accountancy.writa_data(sys.argv[0:], "01zapis.txt")
