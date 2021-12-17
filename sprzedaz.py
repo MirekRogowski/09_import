ca65e35fa64b045613d4f68152b04ba9fce5d8cc
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-17 09:02:23 +0100

    Update sprzedaz.py

--- sprzedaz.py
+++ sprzedaz.py
@@ -1,6 +1,6 @@
 from helper import *
 
 accountancy.open_data(sys.argv[1])
-accountancy.transform_data()
-accountancy.get_sale(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+accountancy.get_sale("sprzedaz", *sys.argv[2:])
 accountancy.writa_data("sprzedaz", "01zapis.txt")
+
