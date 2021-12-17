58c8de2a15718bce256069c941216631cd8ac201
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-17 09:02:26 +0100

    Update zakup.py

--- zakup.py
+++ zakup.py
@@ -1,6 +1,5 @@
 from helper import *
 
 accountancy.open_data(sys.argv[1])
-accountancy.transform_data()
-accountancy.get_buy(sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+accountancy.get_buy("zakup", *sys.argv[2:])
 accountancy.writa_data("zakup", "01zapis.txt")
