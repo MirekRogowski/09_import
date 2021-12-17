bd665f61b44b117031c6723aa0da4dc5b22b2a4f
Rogowski Mirosław <mirek.rogowski@hotmail.com>
2021-12-17 09:02:03 +0100

    Update konto.py

--- konto.py
+++ konto.py
@@ -1,6 +1,5 @@
 from helper import *
 
 accountancy.open_data(sys.argv[1])
-accountancy.get_balance("konto", *sys.argv[2:])
 accountancy.writa_data("konto", "01zapis.txt")
-print(f"\nKonto: {accountancy.balance}")
+
